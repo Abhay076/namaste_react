cdbccb0a537707a10b94e7d65b6a651271fbcd12
Abhay076 <yadavabhaycs@gmail.com>
2024-06-14 12:20:32 +0530

    Episode-01 Inception

--- App.js
+++ App.js
@@ -1,4 +1,32 @@
-const heading = React.createElement('h1',{id:"heading"},"Hello World from React!");
+{
+  /* <div id='parent'>
+    <div id='child'>
+          <h1>I'am h1 Tag</h1>
+          <h2>I'am h1 Tag</h2>
+    </div>
+    <div id='child1'>
+          <h1>I'am h1 Tag</h1>
+          <h2>I'am h1 Tag</h2>
+    </div>
+</div> */
+}
+
+const parent = React.createElement(
+  "div",
+  { id: "parent" },
+ [ React.createElement("div", { id: "child" }, [
+  React.createElement("h1", {}, "I'am h1 Tag"),
+  React.createElement("h2", {}, "I'am h2 Tag"),
+]), React.createElement("div", { id: "child1" }, [
+  React.createElement("h1", {}, "I'am h1 Tag"), 
+  React.createElement("h2", {}, "I'am h2 Tag"),
+])]
+);
+
+// const heading = React.createElement(
+//   "h1",
+//   { id: "heading" },
+//   "Hello World from React!"
+// );
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
-    
+root.render(parent);
