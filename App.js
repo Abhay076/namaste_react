9e4d2f3db13d9cf1165a62431f265e3add114af1
Abhay076 <yadavabhaycs@gmail.com>
2024-06-17 15:06:55 +0530

    Episode-03 | Laying the foundation - babel transpiles

--- App.js
+++ App.js
@@ -1,24 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-
-// React.createElement => ReactElement-JS Object => HTMLElement(render)
-
-const heading = React.createElement(
-  "div",
-  { id: "parent" },
- "Namaste React!"
-);
-
-console.log(heading);
+import React from "react";
+import ReactDOM from "react-dom/client";
 
 //JSX - HTML-Like or XML-like syntax
 
 // JSX (transpiled before it reaches the JS) - PARCEL - Babel
 
-// JSX => React.createElement => ReactElement-JS Object => HTMLElement(render)
-const jsxHeading = <h1 id='heading'>Namaste react using JSX!</h1>
+// JSX => Babel transpiles it to React.createElement => ReactElement-JS Object => HTMLElement(render)
+const jsxHeading = (
+  <h1 className="heading" tabIndex="5">
+    Namaste react using JSX!
+  </h1>
+);
 
-console.log(jsxHeading)
+console.log(jsxHeading);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
