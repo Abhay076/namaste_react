78e726e966665a21ad6813a87fa61073c83a570d
Abhay076 <yadavabhaycs@gmail.com>
2024-06-26 10:39:27 +0530

    Episode-07 | Finding the Path - useEffect

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,16 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { LOGO_URL } from "../utils/constants";
 
 const Header = () => {
   const [btnName, setbtnName] = useState("Login");
+  console.log("header callded");
+
+  // if no dependency[] array => useEffect is called an every render
+  // if dependency array is empty = [] => useEffect is called on initial render(just once means when component render first time)
+  // if dependency array is [btnName] => called everytime btnName is updated
+  useEffect(() => {
+    console.log("useEffect called");
+  }, [btnName]);
 
   return (
     <div className="header">
--- src/components/RestaurantCard.js
+++ src/components/RestaurantCard.js
@@ -7,7 +7,7 @@ const RestaurantCard = (props) => {
     cuisines,
     avgRating,
     costForTwoString,
-    deliveryTime,
+    sla,
   } = resData?.info;
   return (
     <div className="res-card">
@@ -21,7 +21,7 @@ const RestaurantCard = (props) => {
       <h4>{cuisines.join(", ")}</h4>
       <h4>{avgRating} stars</h4>
       <h4>{costForTwoString}</h4>
-      <h4>{deliveryTime} Minutes</h4>
+      <h4>{sla?.slaString}</h4>
     </div>
   );
 };
