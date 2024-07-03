27eaf32d072c854b7c692b7a7b3842ee7513332f
Abhay076 <yadavabhaycs@gmail.com>
2024-07-03 14:41:48 +0530

    Episode-08 | Let's Get Classy - making about section

--- src/components/About.js
+++ src/components/About.js
@@ -1,5 +1,12 @@
+import User from "./User";
 const About = () => {
-  return <h1>About Page</h1>;
+  return (
+    <div>
+      <h1>About Page</h1>
+      <h2>This is Namaste React Web Series</h2>
+      <User />;
+    </div>
+  );
 };
 
 export default About;
