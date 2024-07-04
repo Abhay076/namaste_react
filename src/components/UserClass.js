5de01b2ec8e5027b5b190cee92c1f56d8cecc8c3
Abhay076 <yadavabhaycs@gmail.com>
2024-07-04 11:33:45 +0530

    Episode-08 | Let's Get Classy - class based component

---
+++ src/components/UserClass.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+class UserClass extends React.Component {
+  render() {
+    return (
+      <div className="user-card">
+        <h2>Name: Abhay Yadav</h2>
+        <h3>Location: Gurugram</h3>
+        <h4>Contact: @abhay.yadav_2</h4>
+      </div>
+    );
+  }
+}
+
+export default UserClass;
