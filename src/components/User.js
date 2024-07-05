92c8f264e8ce8d65f00e870334466661fce36574
Abhay076 <yadavabhaycs@gmail.com>
2024-07-05 11:21:37 +0530

    Episode-08 | Let's Get Classy - useState in class component

--- src/components/User.js
+++ src/components/User.js
@@ -1,8 +1,11 @@
+import { useState } from "react";
 const User = (props) => {
   const { name } = props;
+  const [count] = useState(0);
 
   return (
     <div className="user-card">
+      <h3>Count - {count}</h3>
       <h2>Name: {name}</h2>
       <h3>Location: Gurugram</h3>
       <h4>Contact: @abhay.yadav_2</h4>
--- src/components/UserClass.js
+++ src/components/UserClass.js
@@ -3,11 +3,15 @@ import React from "react";
 class UserClass extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      count: 0,
+    };
   }
   render() {
     const { name } = this.props;
     return (
       <div className="user-card">
+        <h3>Count - {this.state.count}</h3>
         <h2>Name: {name}</h2>
         <h3>Location: Gurugram</h3>
         <h4>Contact: @abhay.yadav_2</h4>
