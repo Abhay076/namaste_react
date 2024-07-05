9980f6f9f0292b5a4b4cbc7078cea8d7de16b987
Abhay076 <yadavabhaycs@gmail.com>
2024-07-05 11:25:28 +0530

    Episode-08 | Let's Get Classy - used multiple useState in class component

--- src/components/UserClass.js
+++ src/components/UserClass.js
@@ -5,14 +5,17 @@ class UserClass extends React.Component {
     super(props);
     this.state = {
       count: 0,
+      count2: 1,
     };
   }
   render() {
     const { name } = this.props;
-    const { count } = this.state;
+    const { count, count2 } = this.state;
     return (
       <div className="user-card">
         <h3>Count - {count}</h3>
+        <h3>count2 - {count2}</h3>
+
         <h2>Name: {name}</h2>
         <h3>Location: Gurugram</h3>
         <h4>Contact: @abhay.yadav_2</h4>
